b74277ee65508865a095870a5427c99b55f67f5e
Nazrul <heynazrul@gmail.com>
2023-06-12 11:12:15 +0600

    added admin role verify api

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access!' });
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+};
+
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4zbzvmu.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,6 +49,12 @@ async function run() {
 
     const usersCollection = client.db('rhythmDB').collection('users');
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    });
+
     // users related API
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
@@ -40,11 +68,22 @@ async function run() {
 
       const existingUser = await usersCollection.findOne(query);
       if (existingUser) {
-        return res.send({ message: 'user already exists' });
+        return res.send({ message: 'user exists in user Database' });
       }
-
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    });
+
+    app.get('/users/admin/:email', verifyJWT, async(req, res) => {
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      const result = {admin: user?.role === 'admin'}
+      res.send(result)
     });
 
     app.patch('/users/admin/:id', async (req, res) => {
@@ -56,7 +95,19 @@ async function run() {
           role: 'admin',
         },
       };
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
 
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'instructor',
+        },
+      };
       const result = await usersCollection.updateOne(query, updateDoc);
       res.send(result);
     });
